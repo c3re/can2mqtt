210ab10acc3db0f9c667f3cae9760d37823c9f0e
Malte Muench <mamu@stablerock.de>
2024-04-27 17:18:59 +0200

    All convertmethods with none-function
    Signed-off-by: Malte Muench <mamu@stablerock.de>

---
+++ src/internal/convertfunctions/eightuint82ascii.go
@@ -0,0 +1,16 @@
+package convertfunctions
+
+import "github.com/brutella/can"
+
+func EightUint82AsciiToCan(input string) (can.Frame, error) {
+	var returner [8]byte
+	var i uint8 = 0
+	for ; int(i) < len(input) && i < 8; i++ {
+		returner[i] = input[i]
+	}
+	return can.Frame{Length: i, Data: returner}, nil
+}
+
+func EightUint82AsciiToMqtt(input can.Frame) (string, error) {
+	return string(input.Data[:input.Length]), nil
+}
