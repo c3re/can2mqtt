ee09cb89b02e03e7e9e28ceb34f977fff2d6aa31
Malte Muench <mamu@stablerock.de>
2024-05-06 12:08:06 +0200

    Adds e2e test state
    Signed-off-by: Malte Muench <mamu@stablerock.de>

--- e2e-test/e2e.sh
+++ e2e-test/e2e.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Build
-go build -C ../src -o ../e2e-test/can2mqtt
+go build -C ../src -o ../e2e-test/can2mqtt || exit
 
 # Setup Virtual CAN Interface
 # TODO find something that does not require root privileges
@@ -29,16 +29,16 @@ mosquitto_sub -h localhost -v -t 'e2e-test/#'  > logs/mqtt 2>&1 &
 # Publish MQTT-Tests
 mosquitto_pub -h localhost -m "test" -t e2e-test/none
 mosquitto_pub -h localhost -m "0 0 1 0 1 0 1 1 0 0 1 1 1 1 0 1" -t e2e-test/16bool2ascii
-mosquitto_pub -h localhost -m "test" -t e2e-test/uint82ascii
-mosquitto_pub -h localhost -m "test" -t e2e-test/uint162ascii
-mosquitto_pub -h localhost -m "test" -t e2e-test/uint322ascii
-mosquitto_pub -h localhost -m "test" -t e2e-test/uint642ascii
+mosquitto_pub -h localhost -m "75" -t e2e-test/uint82ascii
+mosquitto_pub -h localhost -m "35000" -t e2e-test/uint162ascii
+mosquitto_pub -h localhost -m "2000000001" -t e2e-test/uint322ascii
+mosquitto_pub -h localhost -m "123470851232" -t e2e-test/uint642ascii
 mosquitto_pub -h localhost -m "0 123441234" -t e2e-test/2uint322ascii
 mosquitto_pub -h localhost -m "12 89 1234 4" -t e2e-test/4uint162ascii
 mosquitto_pub -h localhost -m "-1234 42 1243 2" -t e2e-test/4int162ascii
 mosquitto_pub -h localhost -m "12 2 21 2" -t e2e-test/4uint82ascii
 mosquitto_pub -h localhost -m "1 2 3 4 5 6 7 8" -t e2e-test/8uint82ascii
-mosquitto_pub -h localhost -m "test" -t e2e-test/bytecolor2colorcode
+mosquitto_pub -h localhost -m "#00ff00" -t e2e-test/bytecolor2colorcode
 mosquitto_pub -h localhost -m "12 #00ff00" -t e2e-test/pixelbin2ascii
 
 # Send CAN-Tests
@@ -53,7 +53,7 @@ cansend vcan0 "06b#ABCD"
 cansend vcan0 "06c#ABCD"
 cansend vcan0 "06d#ABCD"
 cansend vcan0 "06e#ABCD"
-cansend vcan0 "06f#ABCD"
+cansend vcan0 "06f#ABCDEF"
 cansend vcan0 "070#ABCD"
 
 #
