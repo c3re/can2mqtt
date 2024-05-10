708db0fce69d2a1c093b369d5ed516d4d44fcfe8
Malte Muench <mamu@stablerock.de>
2024-05-10 15:33:40 +0200

    Adds logging for receivehandling.go
    Signed-off-by: Malte Muench <mamu@stablerock.de>

--- src/internal/receivehandling.go
+++ src/internal/receivehandling.go
@@ -1,9 +1,9 @@
 package internal
 
 import (
-	"fmt"
 	"github.com/brutella/can"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"log/slog"
 )
 
 // handleCAN is the standard receive handler for CANFrames
@@ -11,22 +11,18 @@ import (
 // 1. calling standard convert function: convert2MQTT
 // 2. sending the message
 func handleCAN(cf can.Frame) {
-	if dbg {
-		fmt.Printf("receivehandler: received CANFrame: ID: %d, len: %d, payload %s\n", cf.ID, cf.Length, cf.Data)
-	}
+	slog.Debug("receivehandler: received CANFrame", "id", cf.ID, "len", cf.Length, "data", cf.Data)
 	// Only do conversions when necessary
 	if dirMode != 2 {
 		mqttPayload, err := pairFromID[cf.ID].toMqtt(cf)
 		if err != nil {
-			fmt.Printf("Error while converting CAN Frame with ID %d and payload %s: %s\n", cf.ID, cf.Data, err.Error())
+			slog.Warn("receivehandler: conversion to MQTT message unsuccessful", "convertmode", pairFromID[cf.ID].convMethod, "error", err)
 			return
-		}
-		if dbg {
-			fmt.Printf("receivehandler: converted String: %s\n", mqttPayload)
 		}
 		topic := getTopicFromId(cf.ID)
 		mqttPublish(topic, mqttPayload)
-		fmt.Printf("ID: %d len: %d data: %X -> topic: \"%s\" message: \"%s\"\n", cf.ID, cf.Length, cf.Data, topic, mqttPayload)
+		// this is the most common log-message, craft with care...
+		slog.Info("CAN -> MQTT", "ID", cf.ID, "len", cf.Length, "data", cf.Data, "convertmode", pairFromID[cf.ID].convMethod, "topic", topic, "message", mqttPayload)
 	}
 }
 
@@ -35,22 +31,16 @@ func handleCAN(cf can.Frame) {
 // 1. calling the standard convert function: convert2CAN
 // 2. sending the message
 func handleMQTT(_ MQTT.Client, msg MQTT.Message) {
-	if dbg {
-		fmt.Printf("receivehandler: received message: topic: %s, msg: %s\n", msg.Topic(), msg.Payload())
-	}
+	slog.Debug("receivehandler: received message", "topic", msg.Topic(), "payload", msg.Payload())
 
 	if dirMode != 1 {
-		//cf := convert2CAN(msg.Topic(), string(msg.Payload()))
 		cf, err := pairFromTopic[msg.Topic()].toCan(msg.Payload())
 		if err != nil {
-			fmt.Printf("Error while converting MQTT-Message with Topic %s payload %s: %s\n", msg.Topic(), msg.Payload(), err.Error())
+			slog.Warn("receivehandler: conversion to CAN-Frame unsuccessful", "convertmode", pairFromTopic[msg.Topic()].convMethod, "error", err)
 			return
-		}
-		if dbg {
-			fmt.Printf("receivehandler: converted data: %s\n", cf.Data)
 		}
 		cf.ID = uint32(pairFromTopic[msg.Topic()].canId)
 		canPublish(cf)
-		fmt.Printf("ID: %d len: %d data: %X <- topic: \"%s\" message: \"%s\"\n", cf.ID, cf.Length, cf.Data, msg.Topic(), msg.Payload())
+		slog.Info("CAN <- MQTT", "ID", cf.ID, "len", cf.Length, "data", cf.Data, "convertmode", pairFromTopic[msg.Topic()].convMethod, "topic", msg.Topic(), "message", msg.Payload())
 	}
 }
