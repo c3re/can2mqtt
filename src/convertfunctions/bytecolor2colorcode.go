e088206da528fe11b0fc4a946f8218915b8b922b
Malte Muench <mamu@stablerock.de>
2024-07-28 09:57:58 +0200

    bytecolor2colorcode change to interfaced version

--- src/convertfunctions/bytecolor2colorcode.go
+++ src/convertfunctions/bytecolor2colorcode.go
@@ -8,7 +8,13 @@ import (
 	"strings"
 )
 
-func ByteColor2ColorCodeToCan(input []byte) (can.Frame, error) {
+type ByteColor2ColorCode struct{}
+
+func (_ ByteColor2ColorCode) String() string {
+	return "bytecolor2colorcode"
+}
+
+func (_ ByteColor2ColorCode) ToCan(input []byte) (can.Frame, error) {
 	colorBytes, _ := strings.CutPrefix(string(input), "#")
 	if len(colorBytes) != 6 {
 		return can.Frame{}, errors.New(fmt.Sprintf("input does not contain exactly 6 nibbles each represented by one character, got %d instead", len(colorBytes)))
@@ -22,7 +28,7 @@ func ByteColor2ColorCodeToCan(input []byte) (can.Frame, error) {
 	return returner, nil
 }
 
-func ByteColor2ColorCodeToMqtt(input can.Frame) ([]byte, error) {
+func (_ ByteColor2ColorCode) ToMqtt(input can.Frame) ([]byte, error) {
 	if input.Length != 3 {
 		return []byte{}, errors.New(fmt.Sprintf("Input does not contain exactly 3 bytes, got %d instead", input.Length))
 	}
