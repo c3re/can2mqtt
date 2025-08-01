18df3bbe16e0d6144e0493628d7afce3699731eb
Malte Munch <malte.munch@ltu.se>
2025-08-01 13:16:32 +0200

    Remove all examples
    The examples were mainly temporary thingys that were useful during
    development and might be handy as a snippet here and there in the
    future, that is why I am removing it and dedicating it such a nice
    commit message so in case one neeeds anything of this again, this
    commit with the long message is where its at :)

--- examples/parse_csv.rs
+++
@@ -1,9 +0,0 @@
-use std::process;
-use can2mqtt_rs::config;
-
-fn main() {
-    if let Err(err) = config::parse("example.csv") {
-        println!("error parsing config example: {}", err);
-        process::exit(1);
-    }
-}
