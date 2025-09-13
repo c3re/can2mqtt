f1281f981c9ed22c8361e5eb0d95d433fae17a05
Malte Munch <malte.munch@ltu.se>
2025-09-13 12:06:53 +0200

    Remove Rust examples and (non satisfactory) e2e test
    Signed-off-by: Malte Munch <malte.munch@ltu.se>

--- examples/log.rs
+++
@@ -1,8 +0,0 @@
-use log::*;
-fn main() {
-    stderrlog::new().verbosity(Level::Info).module(module_path!()).init().unwrap();
-    error!("some failure {}, {}", "key", 1); // stuff that will end can2mqtt
-    info!("hihi"); // regular output
-    warn!("moin"); // non-critical error
-    debug!("verbose shit"); // verbose foo
-}
